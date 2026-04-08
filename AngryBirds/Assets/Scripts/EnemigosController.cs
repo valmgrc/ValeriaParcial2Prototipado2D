b05c7bd50d2bbe3e6a4cebe012ee07dfb2c7f307
valmgrc <valeria.mgarcia26@gmail.com>
2026-04-07 23:51:21 -0600

    Corrección de detalles
    Se corrigieron algunos scripts

--- AngryBirds/Assets/Scripts/EnemigosController.cs
+++ AngryBirds/Assets/Scripts/EnemigosController.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 
 public class EnemigosController : MonoBehaviour
 {
-    //public GameObject madera1;
+    
     public TMP_Text textoPuntos;
     static int puntaje = 0;
     public Rigidbody2D cerdito;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.tag == "TablonesMadera")
+        {
         Destroy(gameObject);
         //puntaje = puntaje + 1;
         puntaje++;
         textoPuntos.text = puntaje.ToString();
+        }
     }
 
    
