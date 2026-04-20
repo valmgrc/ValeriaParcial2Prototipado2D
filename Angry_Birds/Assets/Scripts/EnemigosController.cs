280509ce6d5fb17d6f04da116e076a39823e312c
valmgrc <valeria.mgarcia26@gmail.com>
2026-04-20 07:18:17 -0600

    InputActions

--- Angry_Birds/Assets/Scripts/EnemigosController.cs
+++ Angry_Birds/Assets/Scripts/EnemigosController.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EnemigosController : MonoBehaviour
 {
@@ -8,8 +9,42 @@ public class EnemigosController : MonoBehaviour
     static int puntaje = 0;
     public Rigidbody2D cerdito;
 
+    private Controles inputActions;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        inputActions = new Controles();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.PajarosVScerdos.Enable();
+        inputActions.PajarosVScerdos.Presionado.started += LePico; //suscripcion
+    }
+
+    void LePico(InputAction.CallbackContext handler)
+    {
+
+        Debug.Log("Le pico al click izquierdo)");
+
+    }
+
+
+    void NoLePico(InputAction.CallbackContext handler)
+    {
+        Debug.Log("Espacio");
+    }
+
+
+
+
+    private void OnDisable()
+    {
+
+    }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
